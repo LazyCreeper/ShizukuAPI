fe40ae9da6cba6f0c7615f586a7c750e1471a0e7
LazyCreeper <2211717435@qq.com>
2022-01-16 12:50:17 +0800

    Create mcsm-theme.js

---
+++ update/mcsm-theme.js
@@ -0,0 +1,9 @@
+var ver2 = {"mdui": "2.0.1",
+			"mduitext": "1.",
+		   "twopink": "1.1",
+		   "twopinktext": "",
+    "vivid":"1.1",
+    "vividtext":"",
+    "summer":"1.0",
+    "summertext":"",
+}
