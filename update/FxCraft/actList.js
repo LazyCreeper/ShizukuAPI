63a8fcb60865203c6e05698accc6f647740cfe85
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-01-26 22:30:57 +0800

    去p

--- update/FxCraft/actList.js
+++ update/FxCraft/actList.js
@@ -1,6 +1,6 @@
 var actList = [
 	{
 		Date: "2022-1-<span class=id-color>26</span>",
-		Description:"<p>服务器新年活动开始投票，将会从起床战争、建筑大赛、PVP、TNT炸服这四个活动当中投票选出两个。<br><a href=http://shang.qq.com/wpa/qunwpa?idkey=fa65d8a9f30bca3f16567523626872636696b8c8f72745aa273ae02a9f0d60a9>点我加群进行投票</a></p>",
+		Description:"服务器新年活动开始投票，将会从起床战争、建筑大赛、PVP、TNT炸服这四个活动当中投票选出两个。<br><a href=http://shang.qq.com/wpa/qunwpa?idkey=fa65d8a9f30bca3f16567523626872636696b8c8f72745aa273ae02a9f0d60a9>点我加群进行投票</a>",
 	}
 ]
