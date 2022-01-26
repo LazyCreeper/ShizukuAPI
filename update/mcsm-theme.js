55baa0cb750050f400b866bee47994b32c475033
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-01-26 22:16:21 +0800

    emm

--- update/mcsm-theme.js
+++ update/mcsm-theme.js
@@ -4,6 +4,6 @@ var ver2 = {"mdui": "2.0.1",
 		   "twopinktext": "",
     "vivid":"1.1",
     "vividtext":"",
-    "summer":"1.1",
+    "summer":"1.2",
     "summertext":"1. 兼容MCSM 9.1<br>2. 修复一些细节问题",
 }
