3488571757d14ed97097e40b0d51cf1789c6ec6e
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-01-29 17:39:56 +0800

    Update actList.js

--- update/FxCraft/actList.js
+++ update/FxCraft/actList.js
@@ -1,5 +1,9 @@
 var actList = [
 	{
+		Date: "2022-1-<span class=id-color>29</span>",
+		Description:"建筑大赛将于2022年1月30日晚上8点20分举办，胜出者有奖励哦~~<br>另外TNT炸服将在建筑大赛举办完后进行。",
+	},
+	{
 		Date: "2022-1-<span class=id-color>26</span>",
 		Description:"服务器新年活动开始投票，将会从起床战争、建筑大赛、PVP、TNT炸服这四个活动当中投票选出两个。<br><a href=http://shang.qq.com/wpa/qunwpa?idkey=fa65d8a9f30bca3f16567523626872636696b8c8f72745aa273ae02a9f0d60a9>点我加群进行投票</a>",
 	}
