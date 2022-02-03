8dbdc4f9f3c59fa1340afc3bce5f4e3188736575
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-02-03 14:51:50 +0800

    Update mcsm-theme.js

--- update/mcsm-theme.js
+++ update/mcsm-theme.js
@@ -1,9 +1,12 @@
-var ver2 = {"mdui": "2.0.1",
-			"mduitext": "1.",
-		   "twopink": "1.1",
-		   "twopinktext": "",
+var ver2 = {
+	"mdui": "2.0.1",
+	"mduitext": "1.",
+	"twopink": "1.1",
+	"twopinktext": "",
     "vivid":"1.1",
     "vividtext":"",
     "summer":"1.2",
     "summertext":"1. 兼容MCSM 9.1<br>2. 修复一些细节问题",
+	"SakuraPanel":"1.0",
+	"SakuraPanelText":"",
 }
