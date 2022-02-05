bb1228d06700ecc5ca33db6a44c84e626a682b09
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-02-05 17:04:22 +0800

    Update mcsm-theme.js

--- update/mcsm-theme.js
+++ update/mcsm-theme.js
@@ -6,7 +6,7 @@ var ver2 = {
     "vivid":"1.1",
     "vividtext":"",
     "summer":"1.3",
-    "summertext":"1. 兼容MCSM 9.2<br>2. 给终端换了套字体<br>3. 终端改为透明+模糊背景",
+    "summertext":"1. 兼容MCSM 9.2<br>2. 给终端换了套字体<br>3. 终端改为透明背景<br>4. 新增主题设置文件，在<code>/static/setting.css</code>",
 	"SakuraPanel":"1.0",
 	"SakuraPanelText":"",
 }
