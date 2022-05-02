7088621364b1809f20546a240123bd0471b9b78d
Lazy <40060779+LazyCreeper@users.noreply.github.com>
2022-05-02 11:44:11 +0800

    Update mcsm-theme.js

--- update/mcsm-theme.js
+++ update/mcsm-theme.js
@@ -6,7 +6,7 @@ var ver2 = {
     "vivid":"1.1",
     "vividtext":"",
     "summer":"2.3",
-    "summertext":"1. 更新普通用户界面<br> 2. 修复终端字体<br>3. 新增文件管理分页功能<br>4. 优化一些细节问题",
+    "summertext":"1. 更新普通用户界面<br> 2. 修复终端字体<br>3. 新增文件管理分页功能<br>4. 优化一些细节问题<br>警告：如果你是个小白请勿更新到v2.3版本！",
 	"SakuraPanel":"1.0",
 	"SakuraPanelText":"",
 }
